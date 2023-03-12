8f5c4b0930ecd376de79cf8f5caf3be53b5dfdeb
Rytar33 <93471977+Rytar33@users.noreply.github.com>
2023-03-12 12:29:45 +0200

    Add files via upload

--- War-Card-Game-main/reg-check-final.php
+++ War-Card-Game-main/reg-check-final.php
@@ -2,36 +2,45 @@
    if(isset($_COOKIE['name']) and isset($_COOKIE['password']) and isset($_COOKIE['email'])) {
       $to = "maks.Khromakov@mail.ru";
       $from = $_COOKIE['email'];
-
       $message = rand(10000, 99999);
+      setcookie('message', $message, time() + 180);
       $headers = "From: $from". "\r\n" .
       "Reply-To: $from" . "\r\n" . 
       "X-Mailer: PHP/". phpversion();
-      if(mail($to, 'Код подтверждения', "Код: $message", $headers)) {
-         echo "Письмо отправленно.";
-      }
-      if($_POST['code'] == $message) {
+      mail($to, 'Код подтверждения', "Код: $message", $headers);
+   }
+   if(isset($_POST['code'])) {
+      if($_POST['code'] == $_COOKIE['message']) {
          include_once "db.php";
-         $sql = 'INSERT INTO user (name, email, password, backgroundCard) VALUES ('. $_COOKIE['name'] .', '. $_COOKIE['email'] .', '. $_COOKIE['password'] .', "pyro")';
-         $result = $connect->execute($sql);
-         setcookie('password', $_COOKIE['password'], time() - (86400));
-         setcookie('email', $_COOKIE['email'], time() - (86400));
-         $sql = "SELECT * FROM user";
-         $result = $connect->query($sql);
-         while($row = $result->fetch()) {
-            if($row["name"] == $_COOKIE['name']) {
-               setcookie('id', $row["id"], time() + (86400 * 7));
-               setcookie('name', $_COOKIE["name"], time() + (86400 * 6));
-               break;
+         $sql = 'INSERT INTO user (name, email, password, backgroundCard) VALUES (:name, :email, :password, "default")';
+         $result = $connect->prepare($sql);
+         if($result->execute([':name' => $_COOKIE['name'], ':email' => $_COOKIE['email'], ':password' => $_COOKIE['password']])){
+            setcookie('password', $_COOKIE['password'], time() - (86400));
+            setcookie('email', $_COOKIE['email'], time() - (86400));
+            $sql = "SELECT * FROM user";
+            $result = $connect->query($sql);
+            while($row = $result->fetch()) {
+               if($row["name"] == $_COOKIE['name']) {
+                  setcookie('id', $row["id"], time() + (86400 * 7));
+                  setcookie('name', $_COOKIE["name"], time() + (86400 * 6));
+                  setcookie('message', $message, time() - 180);
+                  break;
+               }
             }
+            $sql = 'INSERT INTO statisticswithbot (:id) VALUES (:id)';
+            $result = $connect->prepare($sql);
+            $result->execute([':id' => $_COOKIE['id']]);
+            $sql = 'INSERT INTO statisticswithplayer (:id) VALUES (:id)';
+            $result = $connect->prepare($sql);
+            $result->execute([':id' => $_COOKIE['id']])
+            echo '<script type="text/javascript">location.replace("./mainmenu.php");</script>';
          }
-         header('mainmenu.php');
+         // $result = $connect->execute($sql);
       } else {
-         header('reg-check-final.php');
+         header('Location: reg-check-final.php');
          echo "Неверный код! Повторите попытку.";
       }
    }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,7 +55,7 @@
 <div class="wrapper">
       <div class="form-box" style="width: 600px; height: 350px">
          <div class="form-value">
-            <form action="" method="get">
+            <form action="" method="post">
                <h2>Проверка почты</h2> 
                <div class="input-box mail">
                   <ion-icon name="lock-closed"></ion-icon>
